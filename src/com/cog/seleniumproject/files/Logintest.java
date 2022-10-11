e7443edb67ce5c8310be3edfb4e714966e746c76
PriyadharsiniSankaran <priyadharsini.sankaran@cognizant.com>
2022-10-11 12:42:00 +1100

    Modified the main file

--- src/com/cog/seleniumproject/files/Logintest.java
+++ src/com/cog/seleniumproject/files/Logintest.java
@@ -1,4 +1,5 @@
 package com.cog.seleniumproject.files;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
@@ -16,21 +17,37 @@ public class Logintest {
 		String browsertype="chrome";
 		String url="https://phptravels.net/login";		
 		WebDriver driver;
-     /*   Login lm=new Login(driver);
-        lm.setEmail(email);
-        lm.setPassword(password);*/
+
 		@Test
 		public void send()
 		{
-		driver.get(url);
-		System.out.println("URL ="+driver.getCurrentUrl());
+			driver.get(url);
+			System.out.println("URL ="+driver.getCurrentUrl());
+			/*
+			 * driver.findElement(By.name("email")).sendKeys(email);
+			 * driver.findElement(By.name("password")).sendKeys(password);
+			 * driver.findElement(By.cssSelector(login)).click();
+			 */
+			//driver.findElement(By.cssSelector("a[href='https://www.phptravels.net/account/logout'][class=' waves-effect']")).click();
+
+			
+			  Login lm=new Login(driver);
+			  
+			  lm.setEmail(email); 
+			  System.out.println("Entered email");
+			  lm.setPassword(password);
+			  System.out.println("Entered password");
+			  lm.clickbutton(login);
+			  System.out.println("Logged in Successfully");
+		
+			 
 		}
 		@BeforeTest
 		public void beforetest()
 		{
 			 System.setProperty("webdriver.chrome.driver", "./src/chromedriver");
-			  driver= new ChromeDriver();
-		}
+			 driver= new ChromeDriver();		
+			}
 		@AfterTest
 		public void aftertest()
 		{
